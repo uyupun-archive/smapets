ddbf19c26960b6cb8df879a27b1f050f28d6e4cf
takashi0602 <takashioicsn@gmail.com>
2020-10-03 23:47:10 +0900

    add: setting page

---
+++ src/pages/setting/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss'
+
+const Setting = () => {
+  return (
+    <div>
+      <h1 className={style.title}>設定</h1>
+    </div>
+  );
+}
+
+export { Setting };
--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Top } from './pages/top/';
+import { Setting } from './pages/setting/';
 
 const AppRouter = () => {
   return (
     <div>
       <Router>
         <Route exact path='/' component={Top}/>
+        <Route path='/setting' component={Setting}/>
       </Router>
     </div>
   );
