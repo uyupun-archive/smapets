1f947b7ac01bec6b2f5c6a9ef0343a75cf29d50e
Takahiro Inagaki <inagaki0106@gmail.com>
2020-10-04 07:49:26 +0900

    [change] iconの色などを変更

--- src/components/status/index.js
+++ src/components/status/index.js
@@ -2,23 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLaugh,
-  faFrownOpen,
-  faSadTear,
   faDizzy,
   faBookDead,
+  faMeh,
+  faSmile,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Status = (props) => {
   if (props.ratio >= 80) {
-    return <FontAwesomeIcon icon={faLaugh} color="#fd0000" />;
+    return <FontAwesomeIcon icon={faLaugh} color="#ff00ff" />;
   } else if (props.ratio >= 50) {
-    return <FontAwesomeIcon icon={faFrownOpen} color="#fd0000" />;
+    return <FontAwesomeIcon icon={faSmile} color="#fd0000" />;
   } else if (props.ratio >= 20) {
-    return <FontAwesomeIcon icon={faSadTear} color="#fd0000" />;
+    return <FontAwesomeIcon icon={faMeh} color="#ffd700" />;
   } else if (props.ratio > 0) {
-    return <FontAwesomeIcon icon={faDizzy} color="#fd0000" />;
+    return <FontAwesomeIcon icon={faDizzy} color="#0000cd" />;
   } else {
-    return <FontAwesomeIcon icon={faBookDead} color="#fd0000" />;
+    return <FontAwesomeIcon icon={faBookDead} color="#8a2be2" />;
   }
 };
 
