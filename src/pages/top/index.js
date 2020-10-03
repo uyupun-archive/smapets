c80c733805ce609d7764906bbcf88b8178250308
takashi0602 <takashioicsn@gmail.com>
2020-10-03 22:47:23 +0900

    add: node-sass, style.module.scss

--- src/pages/top/index.js
+++ src/pages/top/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './style.module.scss'
 
 const Top = () => {
   return (
     <div>
-      <h1>トップ</h1>
+      <h1 className={style.title}>トップ</h1>
     </div>
   );
 }
