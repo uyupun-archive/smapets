b96c3240b999d43f6c14fcd4db84a5ce0a9cc1d9
Takahiro Inagaki <inagaki0106@gmail.com>
2020-10-04 00:49:49 +0900

    [add] トップページの名前、HPバーなどを追加

--- src/pages/top/index.js
+++ src/pages/top/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import style from './style.module.scss'
+import React, { useState } from "react";
+import style from "./style.module.scss";
+import StatusIcon from "./images/grin-alt-solid1.svg";
 
 const Top = () => {
+  const [pet, setPet] = useState({
+    name: "いぬ",
+    status: 0,
+    hp: 80,
+    maxHP: 100,
+  });
+
   return (
     <div>
-      <h1 className={style.title}>トップ</h1>
+      <header className={style.header}>
+        <div className={style.header__petName}>{pet.name}</div>
+        <div className={style.header__petStatus}>
+          <Status status={pet.status} />
+        </div>
+        <div className={style.header__petMaxHPBar}>
+          <div
+            className={style.header__petHPBar}
+            style={{ width: pet.hp + "%" }}
+          ></div>
+        </div>
+        <div className={style.header__petHP}>
+          {pet.hp}/{pet.maxHP}
+        </div>
+      </header>
+      <main className={style.main}>pet</main>
     </div>
   );
-}
+};
 
 export { Top };
+
+const Status = (props) => {
+  switch (props.status) {
+    case 0:
+      return <img src={StatusIcon} />;
+  }
+  return <>noimage</>;
+};
