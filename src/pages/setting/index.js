6b7bf10b6134ee8c247efd3c787f4d605dcddb16
Takahiro Inagaki <inagaki0106@gmail.com>
2020-10-04 11:10:55 +0900

    [add] 犬の画像を表示

--- src/pages/setting/index.js
+++ src/pages/setting/index.js
@@ -1,19 +1,21 @@
-import React, {useState} from 'react';
-import style from './style.module.scss';
+import React, { useState } from "react";
+import style from "./style.module.scss";
 import { useHistory } from "react-router-dom";
 import { Client } from "../../service/client/client";
-import { Neko } from "../../assets/images/cats/neko";
+import { Init as NekoInit } from "../../assets/images/cats/neko";
+import { Init as DogInit } from "../../assets/images/dogs/shiba";
 
 const Setting = () => {
   const history = useHistory();
   const [errors, setErrors] = useState({
     kind: {
-      error: false
+      error: false,
     },
     petName: {
-      error: false
+      error: false,
     },
   });
+  const [imageURL, setImageURL] = useState(DogInit);
 
   const setError = (key) => {
     errors[key].error = true;
@@ -24,14 +26,14 @@ const Setting = () => {
     let isError = false;
     if (!e.target.kind.value.trim()) {
       isError = true;
-      setError('kind');
+      setError("kind");
     }
     if (!e.target.petName.value.trim()) {
       isError = true;
-      setError('petName');
+      setError("petName");
     }
     return isError;
-  }
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -50,41 +52,60 @@ const Setting = () => {
       hp: 100,
     });
     history.push("/");
-  }
+  };
 
   return (
-    <div >
+    <div>
       <h1 className={style.title}>ペットを飼う</h1>
       <form className={style.form} onSubmit={onSubmit}>
         <div className={style.form__container}>
           <label>
             <span>しゅるい</span>
-            <select className={style.selectbox} name={"kind"} dafaultvalue={"dog"}>
+            <select
+              className={style.selectbox}
+              name={"kind"}
+              dafaultvalue={"dog"}
+              onChange={(e) => {
+                switch (e.target.value) {
+                  case "犬":
+                    setImageURL(DogInit);
+                    break;
+                  case "猫":
+                    setImageURL(NekoInit);
+                    break;
+                  default:
+                }
+              }}
+            >
               <option dafaultvalue="dog">犬</option>
               <option dafaultvalue="cat">猫</option>
             </select>
           </label>
-          {
-            errors.kind.error && <p className={style.error__message}>選択してください</p>
-          }
+          {errors.kind.error && (
+            <p className={style.error__message}>選択してください</p>
+          )}
         </div>
         <div className={style.form__container}>
           <label>
             <span>おなまえ</span>
-            <input  className={style.formbox} type="textbox" name="petName"/>
+            <input className={style.formbox} type="textbox" name="petName" />
           </label>
-          {
-            errors.petName.error && <p className={style.error__message}>入力してください</p>
-          }
+          {errors.petName.error && (
+            <p className={style.error__message}>入力してください</p>
+          )}
         </div>
         <div className={style.form__container}>
-          <div className={style.image} style={{backgroundImage: `url(${Neko})`}} />
-          {/* TODO: 犬の対応 */}
+          <div
+            className={style.image}
+            style={{ backgroundImage: `url(${imageURL})` }}
+          />
         </div>
-        <button className={style.settingbtn}type="submit">君に決めた！</button>
+        <button className={style.settingbtn} type="submit">
+          君に決めた！
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export { Setting };
